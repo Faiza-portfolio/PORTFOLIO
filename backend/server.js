e6bb20e6c33dcc3d7200e0f66eab6346d2e5ef45
Faiza-portfolio <160414387+faizamehek09@gmail.com.github.com>
2026-03-19 15:51:58 +0530

    updated database connection for Railway

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,11 @@ app.use(bodyParser.json());
 
 // MySQL Database Connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'portfolio_db'
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 // Connect to Database
