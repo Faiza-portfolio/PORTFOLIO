2e788b90840b535612187ea95309322aec9a4cbd
Faiza-portfolio <160414387+faizamehek09@gmail.com.github.com>
2026-03-19 14:06:13 +0530

    updated portfolio files

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',      // CHANGE THIS to your MySQL password
+    password: 'root',
     database: 'portfolio_db'
 });
 
